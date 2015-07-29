0fdecf2cf7f9ffd293c43e654f83e733521d3e6a
ambidextrousTx <ravisinha@my.unt.edu>
2015-07-29 12:37:34 -0500

    Start implementing building bottom layer

--- skiplists/skiplists.c
+++ skiplists/skiplists.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../common/linkedlist.h"
+
+void build_bottom_layer(int array[], int length) {
+	struct Node *head = malloc(sizeof(struct Node));
+
+	for (int i = 0; i < length; i++) {
+		head->element = array[i];
+	}
+}
--- tests/test_skiplists.c
+++ tests/test_skiplists.c
@@ -4,7 +4,8 @@
 
 int main(int argc, const char *argv[])
 {
-	int [] test_array = {1, 3, 4, 5, 7, 9, 10, 11};
+	int test_array[] = {1, 3, 4, 5, 7, 9, 10, 11};
+	build_bottom_layer(test_array, 8);
 
 	return 0;
 }
